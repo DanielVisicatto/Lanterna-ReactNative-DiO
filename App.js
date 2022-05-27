7396c698205a93e2dedccc2e604c79f99093d5e8
Daniel Visicatto <dpfreestyle@gmail.com>
2022-05-27 11:24:16 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(() => {
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+    return () => subscription.remove();
+  }, []);
+
+  return (
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity onPress={handleChangeToggle}>
+        <Image
+          style={style.lightOn}
+          source={
+            toggle
+              ? require('./assets/icons/fire.jpg')
+              : require('./assets/icons/water.jpg')
+          }
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    maxWidth: 710,
+    maxHeight: 710,
+  },
+});
